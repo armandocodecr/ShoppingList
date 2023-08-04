155134605d12c2f48a22abdda0e5d235c76efaf9
ArmandoCode <totovip1464@gmail.com>
2023-08-03 23:25:10 -0600

    use token in apollo client

--- Client/shopping-list-app/src/app/graphql/client.ts
+++ Client/shopping-list-app/src/app/graphql/client.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import Cookies from 'js-cookie'
 
-//const token = Cookies.get('token')
-const token = localStorage.getItem('token')
+const token = localStorage.getItem('token') || ''
 
 export const client = new ApolloClient({
     uri: 'http://localhost:3000/graphql/', // Reemplaza con la URL de tu servidor NestJS
