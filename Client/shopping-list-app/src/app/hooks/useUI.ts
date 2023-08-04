b8636174a599867d64db139f34a4e1f218250403
ArmandoCode <totovip1464@gmail.com>
2023-08-03 23:28:44 -0600

    new states in custom hook in useUI

--- Client/shopping-list-app/src/app/hooks/useUI.ts
+++ Client/shopping-list-app/src/app/hooks/useUI.ts
@@ -3,19 +3,22 @@ import { useUIStore } from "../store/";
 
 export function useUI() {
 
-    const { showAddIteMenu, updateMenuState } = useUIStore(state => ({
-        showAddIteMenu: state.showAddIteMenu,
-        updateMenuState: state.updateMenuState
+    const { showAddIteMenu, updateMenuState, isHistoryMenu, updateHistoryMenuState } = useUIStore(state => ({
+        showAddIteMenu          : state.showAddIteMenu,
+        updateMenuState         : state.updateMenuState,
+        isHistoryMenu           : state.isHistoryMenu,
+        updateHistoryMenuState  : state.updateHistoryMenuState
     }))
 
     return {
 
         //Variables
         showAddIteMenu,
+        isHistoryMenu,
 
         //Methods
-        updateMenuState
-
+        updateMenuState,
+        updateHistoryMenuState
     }
 
 }
