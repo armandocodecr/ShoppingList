abe4cf1ff2371bfeb8a93404ff04d32cbbfc26f2
ArmandoCode <totovip1464@gmail.com>
2023-08-03 23:40:28 -0600

    Move fields

--- Client/shopping-list-app/src/components/Home/index.ts
+++
@@ -1,6 +0,0 @@
-export { CardItem } from "./CardItem";
-export { ContentShoppingList } from "./ContentShoppingList";
-export { AddItem } from "./AddItem";
-export { Menu } from "./Menu";
-export { ListItems } from "./ListItems";
-export { Slogan } from "./Slogan";
