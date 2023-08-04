ecac4afe3ab244dabaf96493aa20a960a752b6a8
ArmandoCode <totovip1464@gmail.com>
2023-08-03 23:41:55 -0600

    Refactor

--- Client/shopping-list-app/src/components/UI/InputSearch.tsx
+++ Client/shopping-list-app/src/components/UI/InputSearch.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { SearchIcon } from "@/app/assets/icons";
 import { useInputsState } from "@/app/hooks";
-
+import { InputComponent } from "./InputComponent";
 
 export function InputSearch() {
 
@@ -10,12 +10,12 @@ export function InputSearch() {
     return(
         <div className="w-80 h-12 bg-[#1b243b] rounded-xl flex justify-between py-2 px-3 items-center shadow-xl">
             <SearchIcon />
-            <input 
-                type="text" 
-                className="w-[85%] bg-transparent pl-3 outline-none text-slate-50" 
+            <InputComponent 
+                className="w-[85%] bg-transparent pl-3 outline-none text-slate-50"
+                typeInput="text"
                 placeholder="Search"
                 value={inputValue}
-                onChange={ev => updateInputValue(ev.target.value)}
+                onChangeFunction={(ev) => updateInputValue(ev.target.value)}
             />
         </div>
     )
