2d4b950753b56ba9463d912cd5488421d8d5f40e
ArmandoCode <totovip1464@gmail.com>
2023-08-05 12:03:15 -0600

    more stores in zustand list state

--- Client/shopping-list-app/src/app/store/lists.ts
+++ Client/shopping-list-app/src/app/store/lists.ts
@@ -1,15 +1,21 @@
 import { create } from 'zustand'
-import { IListSorted } from '../interface/ListInterfaces'
+import { IListSorted, IListsToTremo } from '../interface/ListInterfaces'
 
 interface ILists{
-    dataLists   : IListSorted[] | [],
-    updateLists : (newState : IListSorted[]) => void,
+    dataListsByCreationDate  : IListsToTremo[] | []
+    dataLists                : IListSorted[] | [],
+    updateLists              : (newState : IListSorted[]) => void,
+    updateListsByCreationDate: (newState : IListsToTremo[]) => void,
 }
 
 export const useAllListsStore = create<ILists>(set => 
 ({
     dataLists: [],
+    dataListsByCreationDate: [],
     updateLists: ( newState : IListSorted[]) => set( state => ({
         dataLists: newState
+    })),
+    updateListsByCreationDate: ( newState : IListsToTremo[]) => set( state => ({
+        dataListsByCreationDate: newState
     })),
 }))
