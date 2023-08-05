ee96372bb693fae0f6749fe04118992884423369
ArmandoCode <totovip1464@gmail.com>
2023-08-05 13:12:05 -0600

    add new category querie

--- Client/shopping-list-app/src/app/database/dbCategorys.ts
+++ Client/shopping-list-app/src/app/database/dbCategorys.ts
@@ -1,36 +1,15 @@
 import { client } from '../graphql/client';
-import { addItem, getAllCategorys } from '../graphql/queries';
+import { addCategory, getAllCategorys } from '../graphql/queries';
 import { IDataFromServer } from '../interface/DataInterface';
 
-export const getCategorysFromServer = async() => {
-      
-    // Ejecuta la query utilizando el método 'query' de Apollo Client
-    const result: any = await client
-    .query({
-      query: getAllCategorys,
-    })
-    .then((result) => {
-      // Maneja la respuesta de la query aquí
-      return { ok: true, data: result.data.categories };
-    }).catch(err => {
-      const textError = String(err.graphQLErrors[0].message)
-      return { ok: false, data: textError }
-    })
-
-    const promiseResult: IDataFromServer = result
-    return promiseResult
-  
-}
-
-export const addItemInDB = async( categoryId: string, nameItem: string ) => {
+export const addCategoryInDB = async( nameCategory: string ) => {
 
   const result: any = client
   .mutate({
-    mutation: addItem,
+    mutation: addCategory,
     variables:{
-      createItemInput: {
-        categoryId,
-        name: nameItem,
+      createCategoryInput: {
+        name: nameCategory,
       }
     }
   })
@@ -43,4 +22,25 @@ export const addItemInDB = async( categoryId: string, nameItem: string ) => {
 
   return result
 
+}
+
+export const getCategorysFromServer = async() => {
+      
+    // Ejecuta la query utilizando el método 'query' de Apollo Client
+    const result: any = await client
+    .query({
+      query: getAllCategorys,
+      fetchPolicy: "no-cache"
+    })
+    .then((result) => {
+      // Maneja la respuesta de la query aquí
+      return { ok: true, data: result.data.categories };
+    }).catch(err => {
+      const textError = String(err.graphQLErrors[0].message)
+      return { ok: false, data: textError }
+    })
+
+    const promiseResult: IDataFromServer = result
+    return promiseResult
+  
 }
