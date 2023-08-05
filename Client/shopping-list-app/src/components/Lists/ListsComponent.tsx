420673b6b18e6faa61f78441091a066b8a25cfab
ArmandoCode <totovip1464@gmail.com>
2023-08-05 12:04:00 -0600

    change in validation data in listComponent

--- Client/shopping-list-app/src/components/Lists/ListsComponent.tsx
+++ Client/shopping-list-app/src/components/Lists/ListsComponent.tsx
@@ -1,44 +1,42 @@
-'use client'
-import { useEffect } from "react"
+"use client";
+import { useEffect } from "react";
 
-import { useAllListsStore } from "@/app/store/lists"
+import { useAllListsStore } from "@/app/store/lists";
 
-import { Loader } from "../UI"
+import { Loader } from "../UI";
 
-import { useList } from "@/app/hooks"
-import { CardList } from "./CardList"
+import { useList } from "@/app/hooks";
+import { CardList } from "./CardList";
 
 export function ListsComponent() {
-    
-    const { dataLists } = useAllListsStore(state => ({
-        dataLists: state.dataLists,
-    }))
-    const { getListsToHistory } = useList()
+  const { dataLists } = useAllListsStore((state) => ({
+    dataLists: state.dataLists,
+  }));
+  const { getListsToHistoryAndStatistics } = useList();
 
-    useEffect(() => {
-      
-        getListsToHistory()
+  useEffect(() => {
+    getListsToHistoryAndStatistics();
+  }, []);
 
-    }, [])
-    
-  
-    return (
-        <section className="relative flex flex-col gap-12 min-h-screen pl-32 pt-8 pr-20">
-            <div className="w-full h-auto flex gap-80 self-start">
-                <h1 className="text-2xl text-white tracking-wide mt-3">Shopping history</h1>
-            </div>
+  return (
+    <section className="relative flex flex-col pl-32 pr-16 h-auto overflow-hidden">
+      {dataLists.length === 0 ? (
+        <div className="w-full h-[100%] flex justify-center items-center">
+          <Loader />
+        </div>
+      ) : (
+        <article className="flex gap-11 flex-col">
+          <div className="w-full h-auto flex gap-80 self-start">
+            <h1 className="text-2xl text-white tracking-wide mt-3">
+              Shopping history
+            </h1>
+          </div>
 
-            {
-                dataLists.length === 0 ? (
-                        <div className="absolute w-[80%] h-[100%] flex justify-center items-center">
-                            <Loader />
-                        </div>
-                )
-                :(
-                    <CardList dataLists={ dataLists } />
-                )
-            }
-        </section>
-    )
-
+          <div className="overflow-auto max-h-[calc(90vh-120px)] ScrollCards pr-4">
+            <CardList dataLists={dataLists} />
+          </div>
+        </article>
+      )}
+    </section>
+  );
 }
