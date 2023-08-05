088a0553fd7d8ac25254cafac4076ba57a497784
ArmandoCode <totovip1464@gmail.com>
2023-08-05 12:02:59 -0600

    more stores in zustand items state

--- Client/shopping-list-app/src/app/store/items.ts
+++ Client/shopping-list-app/src/app/store/items.ts
@@ -1,16 +1,29 @@
 import { create } from 'zustand'
 import { IPropertyItems } from '../interface/DataInterface'
+import { IItemsToTremo } from '../interface/ListInterfaces'
 
 interface IItems{
-    dataItems   : IPropertyItems[] | [],
-    updateItems : (newState : IPropertyItems[]) => void,
+    dataItemsToStatistics           : IItemsToTremo[] | [],
+    dataCategoriesToStatistics      : IItemsToTremo[] | [],
+    dataItems                       : IPropertyItems[] | [],
+    updateItems                     : (newState : IPropertyItems[]) => void,
+    updateItemsToStatistics         : (newState : IItemsToTremo[]) => void,
+    updateCategoriesToStatistics    : (newState : IItemsToTremo[]) => void,
 }
 
 export const useAllItems = create<IItems>(set => 
 ({
+    dataItemsToStatistics: [],
+    dataCategoriesToStatistics: [],
     dataItems: [],
     errorMessage: "",
     updateItems: ( newState : IPropertyItems[]) => set( state => ({
         dataItems: newState
+    })),
+    updateItemsToStatistics: ( newState : IItemsToTremo[]) => set( state => ({
+        dataItemsToStatistics: newState
+    })),
+    updateCategoriesToStatistics: ( newState : IItemsToTremo[]) => set( state => ({
+        dataCategoriesToStatistics: newState
     })),
 }))
