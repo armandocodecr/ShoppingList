8cd8e2727e7981c6a2c890a14e2b2dd2bb2f23fd
ArmandoCode <totovip1464@gmail.com>
2023-08-06 21:20:33 -0600

    remove apollo client component to main layout

--- Client/shopping-list-app/src/app/shoppinglist/layout.tsx
+++ Client/shopping-list-app/src/app/shoppinglist/layout.tsx
@@ -6,6 +6,17 @@ import '../globals.css'
 
 import { Navbar } from '@/components/UI'
 import { Menu } from '@/components/Common'
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache, createHttpLink } from '@apollo/client'
+import { client } from '../graphql/client'
+
+export function ApolloClientProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ApolloProvider client={client}>
+      {children}
+    </ApolloProvider>
+  )
+}
+
 
 export const metadata: Metadata = {
   title: 'ShoppingList',
@@ -20,9 +31,11 @@ export default function RootLayout({
 
   return (
         <section className="bg-[#0F172A] flex min-h-screen flex-col items-center justify-center layout">
-            <Navbar />
-            {children}
-            <Menu />
+            <ApolloClientProvider>
+              <Navbar />
+              {children}
+              <Menu />
+            </ApolloClientProvider>
         </section>
   )
 }
