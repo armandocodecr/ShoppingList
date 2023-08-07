299ab665ca044266a23b5aab2745152bbc6de9a6
ArmandoCode <totovip1464@gmail.com>
2023-08-07 11:17:31 -0600

    Add new button to add category

--- Client/shopping-list-app/src/components/Common/Menu.tsx
+++ Client/shopping-list-app/src/components/Common/Menu.tsx
@@ -12,7 +12,7 @@ import { FormikAddCategoryComponent } from "../Form";
 
 export function Menu() {
   const { dataList } = useList();
-  const { showAddIteMenu } = useUI();
+  const { showAddIteMenu, updateCategoryUIState, showAddCategoryUI } = useUI();
   
   return (
     <section className="menu flex-col bg-transparent h-full flex justify-between items-center">
@@ -27,15 +27,31 @@ export function Menu() {
           </div>
         </>
       ) : (
-        <div className="w-full bg-[#1b243b] h-full py-10 px-16 flex items-center flex-col gap-10">
+        <div className="w-full bg-[#1b243b] h-full py-10 px-16 flex items-center flex-col gap-8">
           <h2 className="text-2xl text-slate-50 tracking-wide self-start">
             Add a new item
           </h2>
           <FormikAddItemComponent />
-          <h2 className="text-2xl text-slate-50 tracking-wide self-start">
-            Add a new category
-          </h2>
-          <FormikAddCategoryComponent />
+          {
+            showAddCategoryUI 
+            ?(
+              <div className="flex flex-col gap-8 self-start">
+                <h2 className="text-2xl text-slate-50 tracking-wide self-start">
+                  Add a new category
+                </h2>
+                <FormikAddCategoryComponent />
+              </div>
+            )
+            :(
+              <button 
+                className="h-auto flex justify-center items-center gap-3 self-start"
+                onClick={() => updateCategoryUIState(true)}
+              > 
+                <p className="font-bold text-2xl text-slate-50">+</p>
+                <p className="text-2xl text-slate-50">Add category</p>
+              </button>
+            )
+          }
         </div>
       )}
     </section>
