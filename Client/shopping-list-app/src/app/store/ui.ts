4d42d818915daf5532d8463c4421a0e9c4c6bcff
ArmandoCode <totovip1464@gmail.com>
2023-08-08 15:11:30 -0600

    change ui local state to persist state

--- Client/shopping-list-app/src/app/store/ui.ts
+++ Client/shopping-list-app/src/app/store/ui.ts
@@ -1,26 +1,35 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface IItems{
     showAddCategoryUI       : boolean,
-    showAddIteMenu          : boolean,
+    showAddItem             : boolean,
     isHistoryMenu           : boolean,
     updateMenuState         : (newState : boolean) => void,
     updateHistoryMenuState  : (newState : boolean) => void,
-    updateCategoryUIState  : (newState : boolean) => void,
+    updateCategoryUIState   : (newState : boolean) => void,
 }
 
-export const useUIStore = create<IItems>(set => 
-({
-    showAddCategoryUI: false,
-    showAddIteMenu: false,
-    isHistoryMenu: false,
-    updateMenuState: ( newState : boolean) => set( state => ({
-        showAddIteMenu: newState
-    })),
-    updateHistoryMenuState: ( newState : boolean) => set( state => ({
-        isHistoryMenu: newState
-    })),
-    updateCategoryUIState: ( newState : boolean) => set( state => ({
-        showAddCategoryUI: newState
-    })),
-}))
+export const useUIStore = create<IItems>()(
+persist(
+    set => ({
+        showAddCategoryUI: false,
+        showAddItem: false,
+        isHistoryMenu: false,
+        updateMenuState: value => 
+        set(() => ({
+            showAddItem: value
+        })),
+        updateHistoryMenuState: value => 
+        set(() => ({
+            isHistoryMenu: value
+        })),
+        updateCategoryUIState: value => 
+        set(() => ({
+            showAddCategoryUI: value
+        })),
+    }), 
+    {
+        name: "currentUIMenu",
+    }
+))
